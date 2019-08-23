81b51fd118f24b3bede5f89ab935843c4f92e7b6
Jose Miguel <jmiturralde@miuandes.cl>
2019-08-23 09:58:27 -0400

    Lab 3

---
+++ Solucion Lab 2/Laboratorio_2_OOP_201902/Board.cs
@@ -0,0 +1,116 @@
+﻿using Laboratorio_2_OOP_201902.Card;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratorio_2_OOP_201902
+{
+    public class Board
+    {
+        //Constantes
+        private const int DEFAULT_NUMBER_OF_PLAYERS = 2;
+
+        //Atributos
+        private Dictionary<string, List<Card.Card>>[] playerCards;
+
+        private List<SpecialCard> weatherCards;
+
+        //Propiedades
+       public Dictionary<string, List<Card.Card>>[] PlayerCards
+        {
+            get
+            {
+                return this.playerCards;
+            }
+
+        }
+        public List<SpecialCard> WeatherCards
+        {
+            get
+            {
+                return this.weatherCards;
+            }
+        }
+
+
+        //Constructor
+       
+        public Board()
+        {
+            this.weatherCards = new List<SpecialCard>();
+            this.playerCards = new Dictionary<string, List<Card.Card>>[DEFAULT_NUMBER_OF_PLAYERS]; //Inicializa el arreglo de diccionarios.
+            this.playerCards[0] = new Dictionary<string, List<Card.Card>>(); //Inicializa los diccionarios.
+            this.playerCards[1] = new Dictionary<string, List<Card.Card>>(); //Inicializa los diccionarios.
+        }
+
+
+
+
+    //Metodos
+
+     public void AddCard(Card.Card card, int playerId = -1, string buffType = null)
+        {
+            if (card.GetType().Name == nameof(CombatCard))
+            {
+                if (playerId ==0|| PlayerId == 1)
+                {
+                    foreach ()
+                }
+            }
+        }
+   
+        public void DestroyCombatCards()
+        {
+            this.meleeCards = new List<CombatCard>[DEFAULT_NUMBER_OF_PLAYERS];
+            this.rangeCards = new List<CombatCard>[DEFAULT_NUMBER_OF_PLAYERS];
+            this.longRangeCards = new List<CombatCard>[DEFAULT_NUMBER_OF_PLAYERS];
+        }
+        public void DestroySpecialCards()
+        {
+            this.specialMeleeCards = new SpecialCard[DEFAULT_NUMBER_OF_PLAYERS];
+            this.specialRangeCards = new SpecialCard[DEFAULT_NUMBER_OF_PLAYERS];
+            this.specialLongRangeCards = new SpecialCard[DEFAULT_NUMBER_OF_PLAYERS];
+            this.weatherCards = new List<SpecialCard>();
+        }
+        public int[] GetMeleeAttackPoints()
+        {
+            //Debe sumar todos los puntos de ataque de las cartas melee y retornar los valores por jugador.
+            int[] totalAttack = new int[] { 0, 0 };
+            for (int i=0; i < 2; i++)
+            {
+                foreach (CombatCard meleeCard in meleeCards[i])
+                {
+                    totalAttack[i] += meleeCard.AttackPoints;
+                }
+            }
+            return totalAttack;
+            
+        }
+        public int[] GetRangeAttackPoints()
+        {
+            //Debe sumar todos los puntos de ataque de las cartas range y retornar los valores por jugador.
+            int[] totalAttack = new int[] { 0, 0 };
+            for (int i = 0; i < 2; i++)
+            {
+                foreach (CombatCard rangeCard in rangeCards[i])
+                {
+                    totalAttack[i] += rangeCard.AttackPoints;
+                }
+            }
+            return totalAttack;
+        }
+        public int[] GetLongRangeAttackPoints()
+        {
+            //Debe sumar todos los puntos de ataque de las cartas longRange y retornar los valores por jugador.
+            int[] totalAttack = new int[] { 0, 0 };
+            for (int i = 0; i < 2; i++)
+            {
+                foreach (CombatCard longRangeCard in longRangeCards[i])
+                {
+                    totalAttack[i] += longRangeCard.AttackPoints;
+                }
+            }
+            return totalAttack;
+        }
+    }
+}
---
+++ Solucion Lab 2/Laboratorio_2_OOP_201902/Deck.cs
@@ -0,0 +1,31 @@
+﻿using Laboratorio_2_OOP_201902.Card;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratorio_2_OOP_201902
+{
+    public class Deck
+    {
+        List<Card.Card> cards;
+
+        public Deck()
+        {
+            cards = new List<Card.Card>();
+        }
+
+        public void AddCombatCard(CombatCard combatCard)
+        {
+            cards.Add(combatCard);
+        }
+        public void AddSpecialCard(SpecialCard specialCard)
+        {
+            cards.Add(specialCard);
+        }
+        public void DestroyCombatCard(int cardId) { throw new NotImplementedException(); }
+        public void DestroySpecialCard(int cardId) { throw new NotImplementedException(); }
+        public void Shuffle() { 
+            throw new NotImplementedException();
+        }
+    }
+}
